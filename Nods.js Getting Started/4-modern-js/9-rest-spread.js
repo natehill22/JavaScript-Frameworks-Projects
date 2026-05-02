0235c5ffbd236086ec014dd6a99c0afecc7bbeed
natehill22 <natehill22@gmail.com>
2026-05-01 17:23:20 -0700

    Uploading node.js workbook files

---
+++ Nods.js Getting Started/4-modern-js/9-rest-spread.js
@@ -0,0 +1,22 @@
+const [first, ...restOfItems] = [10, 20, 30, 40];
+
+const data = {
+  temp1: '001',
+  temp2: '002',
+  firstName: 'John',
+  lastName: 'Doe',
+  address: {
+    city: 'Tucson',
+    state: 'Arizona'
+  }
+};
+
+const { temp1, temp2, ...person } = data;
+
+const newArray = [...restOfItems];
+
+const newObject = {
+  ...person,
+};
+
+const trimmedData = { ...person };
