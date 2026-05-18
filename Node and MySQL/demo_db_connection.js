ff17fe41c7b179287195cb9889df9024c160e333
natehill22 <natehill22@gmail.com>
2026-05-18 16:05:29 -0700

    Security update

--- Node and MySQL/demo_db_connection.js
+++ Node and MySQL/demo_db_connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Cheese4me"
+    password: ""
 });
 
 con.connect(function(err) {
