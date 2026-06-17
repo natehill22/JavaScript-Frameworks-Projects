83af910edf11129ebf4d5739a980fab6adaeba5a
natehill22 <natehill22@gmail.com>
2026-06-16 17:11:28 -0700

    Uploading new/updated files in the NG: Getting Started Tutorial files

--- Angular Getting Started/APM/src/app/products/product-list.component.ts
+++ Angular Getting Started/APM/src/app/products/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { IProduct } from './product';
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub!: Subscription;
   
   private _listFilter: string = '';
   get listFilter(): string {
@@ -41,8 +43,17 @@ export class ProductListComponent implements OnInit {
 
   //Using a lifecycle hook to print a message to the console upon component startup
   ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();    
   }
 
   onRatingClicked(message: string): void {
