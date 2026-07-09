398b017660f301f97676bd8cbbd1fdb36018706c
natehill22 <natehill22@gmail.com>
2026-07-08 17:25:31 -0700

    Adding sass file to react folder and adding new react project

--- React/mysecondreact/src/index.js
+++ React/mysecondreact/src/index.js
@@ -33,7 +33,7 @@ class B extends A {
 
 console.log(new B({title: 'hello world'}))
 */
-
+/*
 import { createRoot } from 'react-dom/client'
 
 function Element(props) {
@@ -57,4 +57,113 @@ function Fave() {
 
 createRoot(document.getElementById('root')).render(
   <Fave />,
-)
+) 
+*/
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import './mysass.scss';
+
+class Fruit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Durian",
+      adj1: "repulsive",
+      adj2: "yellow",
+      origin: "Southeast Asia",
+      show: true,
+      isTargetOn: false
+    };
+
+  //Explicitly binds 'this' to the class method
+  this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState((prevState) => ({
+      isTargetOn: !prevState.isTargetOn
+    }));
+  }
+
+  delSlander = () => {
+    this.setState({show: false});
+  }
+  /*
+  static getDerivedStateFromProps(props, state) {
+    return {adj1: props.adjSwitch};
+  }
+  */
+  
+  shouldComponentUpdate() {
+    return true;
+  }
+  changeAdjective = () => {
+    this.setState({adj1: "spiky"});
+  }
+  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({adj1: "stinky"})
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML = 
+    "Before the update, the adjective was " + prevState.adj1;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML = 
+    "The updated adjective is " + this.state.adj1;
+  }
+  render() {
+    let durianopinions;
+    if (this.state.show) {
+      durianopinions = <Opinion />;
+    };
+    return (
+      <div>
+        <h1>My {this.state.name}</h1>
+        <p>
+          It is a {this.state.adj1}&nbsp;
+          {this.state.adj2} fruit
+          from {this.state.origin}.
+        </p>
+        <button 
+          type="button" 
+          onClick={this.changeAdjective}
+        >Change Adjective</button>
+        <div id="div1"></div>
+        <div id="div2"></div>
+        <div>
+          {durianopinions}
+          <button type="button" onClick={this.delSlander}>Take that back!</button>
+        </div>
+        <div>
+        <p>The switch is {this.state.isTargetOn ? 'ON' : 'OFF'}</p>
+        {/* Passes the bound method to the onClick attribute */}
+        <button onClick={this.handleToggle}>
+          Toggle
+        </button>
+      </div>
+      </div>
+    );
+  }
+}
+
+class Opinion extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Durian Opinion is about to be unmounted.");
+  }
+  render() {
+    return (
+      <h3>Durian is the worst!!</h3>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Fruit />);
+
+// Create a root node and render your Fruit component into it
+//createRoot(document.getElementById('root')).render(
+//  <Fruit />, 
+//);
