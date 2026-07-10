2c0c7fa391ae9744f5a259bd8a50fe7033f6c42e
natehill22 <natehill22@gmail.com>
2026-07-09 17:22:59 -0700

    Adding comments to StylingReactComponents files

--- React/StylingReactComponents/src/components/Newsletter.js
+++ React/StylingReactComponents/src/components/Newsletter.js
@@ -3,11 +3,12 @@ import React from "react";
 import css from "./Newsletter.module.css";
 
 export default function Newsletter() {
-  const [email, setEmail] = React.useState("");
-  const emailPartsCount = countEmailParts(email);
+  const [email, setEmail] = React.useState(""); //Stores raw string from input field
+  const emailPartsCount = countEmailParts(email); //Calculates score from 0-5 on every keystroke
   return (
     <Container>
       <Spectrum aria-hidden>
+        {/* Maps over 5 indicator bars and renders a row of them, if conditions are met */}
         {Array.from(Array(5)).map((_, i) => (
           <Bar active={i + 1 <= emailPartsCount} key={i}></Bar>
         ))}
@@ -15,17 +16,20 @@ export default function Newsletter() {
       <Header>
         <h2>Get the newsletter</h2>
       </Header>
+      {/* Renders the input field that updates email state upon change */}
       <Email
         type="email"
         placeholder="Your email"
         value={email}
         onChange={(evt) => setEmail(evt.target.value)}
       />
+      {/* Renders the sign-up button (it will switch CSS classes to active state when emailPartsCount reaches 5) */}
       <Submit active={emailPartsCount >= 5}>Sign up</Submit>
     </Container>
   );
 }
 
+//Creates React components that capture any elements within them and apply CSS class names
 function Container(props) {
   return <section className={css.container}>{props.children}</section>;
 }
@@ -58,6 +62,7 @@ function Bar(props) {
   return <div className={props.active ? css.barActive : css.bar}></div>;
 }
 
+//Determines how many steps of the email pattern are completed using Regex
 function countEmailParts(email) {
   if (/@.+\..{2,}$/.test(email)) {
     return 5;
