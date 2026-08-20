dbd75e3132f7cb154b25c653f033feaddf5bf89b
natehill22 <natehill22@gmail.com>
2026-08-20 15:23:08 -0700

    Modernizing controller for nodejs-espress-mongodb project
    -Switching to async/await instead of .then() .catch() blocks
    -Updating the update function to protect against payload injection security risk
    -Improving error handling for invalid mongoDB ids
    -Switched to Express Error middleware instead of the same copy/pasted catch block

--- nodejs-express-mongodb/app/controllers/tutorial.controller.js
+++ nodejs-express-mongodb/app/controllers/tutorial.controller.js
@@ -1,155 +1,142 @@
 const db = require("../models"); //Pulls in the db object from index.js
+const mongoose = require("mongoose"); //Imports mongoose to help with database functionality
 const Tutorial = db.tutorials; //Sets all db model/functionality to the Tutorial variable so it can be used to query MongoDB
 
 //Creates and Saves a new Tutorial
-exports.create = (req, res) => {
-    //Returns an error message/status and stops execution if req.body.title doesn't exist
-    if (!req.body.title) { 
-        res.status(400).send({ message: "Content can not be empty!" });
-        return;
-    }
+exports.create = async (req, res, next) => {
+    try {
+        const { title, description, published } = req.body; //Creates three variables filled with req.body data
 
-    //Creates a schema-matching Tutorial object from the entered data 
-    const tutorial = new Tutorial({
-        title: req.body.title,
-        description: req.body.description,
-        published: req.body.published ? req.body.published : false //If published is empty, defaults to false
-    });
+        //Returns an error message/status and stops execution if req.body.title doesn't exist
+        if (!title) { 
+            return res.status(400).send({ message: "Title can not be empty!" });
+        }
 
-    //Saves Tutorial in the database 
-    tutorial
-    .save(tutorial) //Pushes new document to the database 
-    .then(data => {
-        res.send(data); //Returns the newly created tutorial object
-    })
-    //If creation fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while creating the Tutorial"
+        //Creates a schema-matching Tutorial object from the entered data 
+        const tutorial = new Tutorial({
+            title,
+            description,
+            published: published === true ?? false //If published is empty, defaults to false
         });
-    });
+
+        //Saves Tutorial in the database 
+        const data = await tutorial.save(); //Pushes new document to the database 
+        res.status(201).send(data); //Returns the newly created tutorial object with status
+    } catch (err) { 
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Retrieves all Tutorials from the database
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res, next) => {
+    try {
     //Pulls query string out of the url to be used as a condition for the findAll method
     const title = req.query.title;
     var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {}; //MongoDB regex query with case-insensitivity, if no title an empty object is used to match everything
 
     //Queries the database
-    Tutorial.find(condition)
-    .then(data => {
-        res.send(data); //Returns the list of tutorials found 
-    })
-    //If retrieval fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving tutorials"
-        });
-    });
+    const data = await Tutorial.find(condition);
+    res.send(data); //Returns the list of tutorials found 
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Finds a single Tutorial with an id
-exports.findOne = (req, res) => {
-    const id = req.params.id; //Grabs the id from the URL path
+exports.findOne = async (req, res, next) => {
+    try {
+        const id = req.params.id; //Grabs the id from the URL path
 
-    //Searches specifically for that matching id
-    Tutorial.findById(id)
-    .then(data => {
-        if (!data)
-            res.status(404).send({ message: "Not found Tutorial with id " + id }); //Sends error message/status if can't be found
-        else res.send(data); //Return tutorial data if found
-    })
-    //If retrieval fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message: "Error retrieving Tutorial with id=" + id });
-    });
+        //Validates if ID format is acceptable by mongoDB
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).send({ message: "Invalid ID format" });
+        }
+        //Searches specifically for that matching id
+        const data = await Tutorial.findById(id);
+        if (!data) {
+            return res.status(404).send({ message: `Tutorial not found with id ${id}` }); //Sends error message/status if can't be found
+        }
+        res.send(data); //Return tutorial data if found
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Updates a Tutorial by the id in the request
-exports.update = (req, res) => {
-    //Returns an error message/status if req.body doesn't exist
-    if (!req.body) {
-        return res.status(400).send({
-            message: "Data to update can not be empty!"
+exports.update = async (req, res, next) => {
+    try {
+        const id = req.params.id; //Grabs the id from the URL path
+
+        //Returns an error message/status if not valid for mongo
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).send({ message: "Invalid ID format provided" });
+        }
+
+        const { title, description, published } = req.body; //Creates three variables filled with req.body data
+        const updateData = {};
+        //Adds fields to updateData object only if they exist
+        if (title !== undefined) updateData.title = title;
+        if (description !== undefined) updateData.description = description;
+        if (published !== undefined) updateData.published = published;
+
+        //Locates document by its id and applies new fields from updateData
+        const data = await Tutorial.findByIdAndUpdate(id, updateData, { 
+            new: true,
+            runValidators: true //Validates data againt mongo's schema
         });
-    }
 
-    const id = req.params.id; //Grabs the id from the URL path
-
-    //Locates document by its id and applies new fields from req.body
-    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
         if (!data) {
-            res.status(404).send({ //Sends error message/status if can't be found
-                message: `Cannot update Tutorial with id=${id}. Tutorial was not found!`
-            });
-        } else res.send({ message: "Tutorial was updated successfully." }); //Return update message if found
-    })
-    //If update fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message: "Error updating Tutorial with id=" + id });
-    });
+            //Sends error message/status if can't be found
+            return res.status(404).send({ message: `Cannot update Tutorial with id=${id}. Tutorial was not found!` });
+        } 
+
+        res.send({ message: "Tutorial was updated successfully.", data }); //Return update message if found
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Deletes a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-    const id = req.params.id; //Grabs the id from the URL path
+exports.delete = async (req, res, next) => {
+    try {
+        const id = req.params.id; //Grabs the id from the URL path
     
-    //Locates document by its id and deletes it
-    Tutorial.findByIdAndDelete(id)
-    .then(data => {
-        if (!data) {
-            res.status(404).send({ //Sends error message/status if can't be found
-                message: `Cannot delete Tutorial with id=${id}. Tutorial was not found!`
-            });
-        } else {
-            res.send({ //Return deletion successful message if found
-                message: "Tutorial was deleted successfully!"
-            });
+        //Returns an error message/status if not valid for mongo
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).send({ message: "Invalid ID format provided" });
         }
-    })
-    //If delete fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message: "Could not delete Tutorial with id=" + id });
-    });
+
+        //Locates document by its id and deletes it
+        const data = await Tutorial.findByIdAndDelete(id);
+        if (!data) {
+            //Sends error message/status if can't be found
+            return res.status(404).send({ message: `Cannot delete Tutorial with id=${id}. Tutorial was not found!` });
+        } 
+        
+        res.send({ message: "Tutorial was deleted successfully!" }); //Return deletion successful message if found
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Deletes all Tutorials from the database
-exports.deleteAll = (req, res) => {
-    //Uses an empty filter to match all items--deletes all tutorial documents
-    Tutorial.deleteMany({})
-    .then(data => {
-        res.send({
-            message: `${data.deletedCount} Tutorials were deleted successfully!`
-        }); //Return deletion successful message with count
-    })
-    //If delete fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while removing all tutorials"
-        });
-    });
+exports.deleteAll = async (req, res, next) => {
+    try {
+        //Uses an empty filter to match all items--deletes all tutorial documents
+        const data = await Tutorial.deleteMany({});
+        res.send({ message: `${data.deletedCount} Tutorials were deleted successfully!` }); //Return deletion successful message with count
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
 
 //Finds all published Tutorials
-exports.findAllPublished = (req, res) => {
-    //Locates all published Tutorial documents and returns them
-    Tutorial.find({ published: true })
-    .then(data => {
+exports.findAllPublished = async (req, res, next) => {
+    try {
+        //Locates all published Tutorial documents and returns them
+        const data = await Tutorial.find({ published: true });
         res.send(data); //Returns list of published Tutorial documents
-    })
-    //If retrieval fails, return error message/status
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving published tutorials"
-        });
-    });
+    } catch (err) {
+        next(err); //If creation fails, return error message
+    }
 };
